0c908f18aaba43eb8d400cdf3852aa0e366ea0fd
mohammad-makhamreh <mohammadmakhamrah@gmail.com>
2024-08-19 13:30:28 +0300

    First commit of the project after finishing the login popup.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let popup = document.getElementById("sign-up");
+
+function openPopup() {
+    popup.classList.add("open-sign-up");
+}
+
+
+function closePopup() {
+    popup.classList.remove("open-sign-up");
+    popup.classList.add("sign-up");
+}
